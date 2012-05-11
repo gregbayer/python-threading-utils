2584ac2bf7ca6c292dabd3f47f281a22e2b724c5
Greg Bayer <greg@gbayer.com>
2012-05-11 09:29:53 -0700

    added ThreadedWorker

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-python-threading-utils
-======================
+# About
 
-Basic helpers for working with threads in Python.
+As the name implies, python-threading-utils is a set of helper classes and functions that make it easier to start using threads in Python.
+
+* ThreadedWorker - Makes it easy to create a thread that will pull work from a syncronized work_queue and write it to a syncronized result_queue.
+* start_logging_with_thread_info - Helper to add thread id to all log messages. (assumes there is a root logging handler already)
+* stop_logging_with_thread_info - Helper to remove thread id from all log messages. (assumes there is a root logging handler already)
+
+# ThreadedWorker Example
+
+    import Queue
+    
+    urls_to_process = ["http://url1.com", "http://url2.com", "http://site1.com", "http://site2.com"]
+    
+    work_queue = Queue.Queue()
+    result_queue = Queue.Queue()
+
+    def process_url(url):
+        # TODO: Do some work with the url
+        return url
+    
+    def main():
+        # spawn a pool of threads, and pass them queue instance 
+        for i in range(5):
+            t = ThreadedWorker(work_queue, result_queue, work_func=process_url)
+            t.setDaemon(True)
+            t.start()
+            
+        # populate queue with data   
+        for url in urls_to_process:
+            work_queue.put(url)
+            
+        # wait on the queue until everything has been processed     
+        work_queue.join()
+        
+        # print results
+        print repr(result_queue)
+    
+    main()
+
+
+# Dependencies: 
+* [python threading](http://docs.python.org/library/threading.html)
+* [python Queue](http://docs.python.org/library/queue.html)
+
+
+# References
+* [ibm article on python threading](http://www.ibm.com/developerworks/aix/library/au-threadingpython/)
---
+++ src/thread_helper.py
@@ -0,0 +1,86 @@
+import threading
+import logging
+import sys
+
+class ThreadedWorker(threading.Thread):
+    """ Generic Threaded Worker
+        Input to work_func: item from work_queue
+    
+    Example usage:
+    
+    import Queue
+    
+    urls_to_process = ["http://url1.com", "http://url2.com", "http://site1.com", "http://site2.com"]
+    
+    work_queue = Queue.Queue()
+    result_queue = Queue.Queue()
+
+    def process_url(url):
+        # TODO: Do some work with the url
+        return url
+    
+    def main():
+        # spawn a pool of threads, and pass them queue instance 
+        for i in range(5):
+            t = ThreadedWorker(work_queue, result_queue, work_func=process_url)
+            t.setDaemon(True)
+            t.start()
+            
+        # populate queue with data   
+        for url in urls_to_process:
+            work_queue.put(url)
+            
+        # wait on the queue until everything has been processed     
+        work_queue.join()
+        
+        # print results
+        print repr(result_queue)
+    
+    main()
+    """
+
+    def __init__(self, work_queue, result_queue, work_func, stop_when_work_queue_empty=True, queue_timeout=1):
+        threading.Thread.__init__(self)
+        self.work_queue = work_queue
+        self.result_queue = result_queue
+        self.work_func = work_func
+        self.stop_when_work_queue_empty = stop_when_work_queue_empty
+        self.queue_timeout = queue_timeout
+
+
+    def should_continue_running(self):
+        if self.stop_when_work_queue_empty:
+            return not self.work_queue.empty()
+        else:
+            return True
+
+
+    def run(self):
+        while self.should_continue_running():
+            try:
+                # grabs item from work_queue
+                work_item = self.work_queue.get(timeout=self.queue_timeout)
+                
+                # works on item
+                work_result = self.work_func(work_item)
+                
+                #place work_result into result_queue
+                self.result_queue.put(work_result, timeout=self.queue_timeout)
+                
+                #signals to work_queue that item is done
+                self.work_queue.task_done()
+        
+            except:
+                logging.exception('Error in ThreadedWorker')
+
+
+def start_logging_with_thread_info():
+    formatter = logging.Formatter('[thread %(thread)-3s] %(message)s')
+    logging.getLogger().handlers[0].setFormatter(formatter)
+
+
+def stop_logging_with_thread_info():
+    formatter = logging.Formatter('%(message)s')
+    logging.getLogger().handlers[0].setFormatter(formatter)
+
+
